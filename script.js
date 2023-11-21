5d00e2699d4bfb34298b5533ca172013e9a868b6
paulaanb <pnarabor@myuax.com>
2023-11-21 19:50:29 +0100

    Ya funcionan todos los botones

--- script.js
+++ script.js
@@ -1,69 +1,11 @@
 // script.js
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Personalización del Usuario
-  document.getElementById('personalizarBtn').addEventListener('click', function() {
-    var nombreUsuario = prompt('Por favor, ingresa tu nombre:');
-    if (nombreUsuario !== null && nombreUsuario !== '') {
-      document.getElementById('nombreUsuario').innerText = '¡Hola, ' + nombreUsuario + '!';
-      document.getElementById('preguntas').style.display = 'block';
-    }
-  });
-
-  // Recopilar información sobre preferencias del usuario
-  document.getElementById('finalizarBtn').addEventListener('click', function() {
-    var genero = document.getElementById('genero').value;
-    var plataforma = document.getElementById('plataforma').value;
-
-    // Realizar recomendaciones según las preferencias del usuario
-    if (genero === 'accion' && plataforma === 'netflix') {
-      alert('Te recomendamos películas de acción en Netflix. ¡Disfruta!');
-      redirigir('https://www.netflix.com/browse/genre/1365');
-    } else if (genero === 'comedia' && plataforma === 'amazon') {
-      alert('Te recomendamos comedias en Amazon Prime Video. ¡Diviértete!');
-      redirigir('https://www.amazon.com/gp/video/storefront');
-    } else if (genero === 'drama' && plataforma === 'cine') {
-      alert('Te recomendamos dramas en el cine. ¡Prepárate para una experiencia emotiva!');
-      redirigir('https://www.google.com/search?q=dramas+en+cine');
-    } else {
-      alert('Gracias por personalizar tu experiencia. ¡Disfruta del cine!');
-    }
-  });
-
-  // Función para redirigir a la URL proporcionada
-  function redirigir(url) {
-    window.location.href = url;
-  }
-
-  // Cambios de Color
-  document.getElementById('cambiarColorBtn').addEventListener('click', function() {
-    var colores = ['#ffcccb', '#b3e6ff', '#ffd700', '#c2f0c2'];
-    var colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-    document.body.style.backgroundColor = colorAleatorio;
-  });
-});
-// Función para cambiar el color de fondo
 function cambiarColorFondo() {
   var colores = ['#ffcccb', '#b3e6ff', '#ffd700', '#c2f0c2'];
   var colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
   document.body.style.backgroundColor = colorAleatorio;
 }
-// Función para mostrar un elemento por ID
-function mostrarElemento(id) {
-  var elemento = document.getElementById(id);
-  if (elemento) {
-    elemento.style.display = 'block';
-    console.log('Elemento mostrado');
-  }
-}
-// Función para ocultar un elemento por ID
-function ocultarElemento(id) {
-  var elemento = document.getElementById(id);
-  if (elemento) {
-    elemento.style.display = 'none';
-  }
-}
-// Agregar noticias al hacer clic en el botón
+
 function agregarNoticia() {
   var tituloNoticia = prompt('Ingrese el título de la noticia:');
   var contenidoNoticia = prompt('Ingrese el contenido de la noticia:');
@@ -72,10 +14,10 @@ function agregarNoticia() {
     // Crea un nuevo elemento de noticia
     var nuevaNoticia = document.createElement('div');
     nuevaNoticia.className = 'noticia';
-    
+
     // Agrega el contenido de la noticia
     nuevaNoticia.innerHTML = `<h3>${tituloNoticia}</h3><p>${contenidoNoticia}</p>`;
-    
+
     // Agrega la noticia al contenedor de noticias
     var contenedorNoticias = document.getElementById('contenedorNoticias');
     contenedorNoticias.appendChild(nuevaNoticia);
@@ -84,3 +26,53 @@ function agregarNoticia() {
   }
 }
 
+function mostrarOcultarElemento(id) {
+  var elemento = document.getElementById(id);
+  if (elemento) {
+    if (elemento.style.display === 'none' || elemento.style.display === '') {
+      elemento.style.display = 'block';
+    } else {
+      elemento.style.display = 'none';
+    }
+  }
+}
+
+function iniciarPersonalizacion() {
+  var nombreUsuario = prompt('Por favor, ingresa tu nombre:');
+  if (nombreUsuario !== null && nombreUsuario !== '') {
+    document.getElementById('nombreUsuario').innerText = '¡Hola, ' + nombreUsuario + '!';
+    document.getElementById('preguntas').style.display = 'block';
+  }
+}
+
+function finalizarPersonalizacion() {
+  var genero = document.getElementById('genero').value;
+  var plataforma = document.getElementById('plataforma').value;
+
+  if (genero === 'accion' && plataforma === 'netflix') {
+    alert('Te recomendamos películas de acción en Netflix. ¡Disfruta!');
+    redirigir('https://www.netflix.com/browse/genre/1365');
+  } else if (genero === 'comedia' && plataforma === 'amazon') {
+    alert('Te recomendamos comedias en Amazon Prime Video. ¡Diviértete!');
+    redirigir('https://www.amazon.com/gp/video/storefront');
+  } else if (genero === 'drama' && plataforma === 'hbo') {
+    alert('Te recomendamos dramas en HBO. ¡Prepárate para una experiencia emotiva!');
+    redirigir('https://www.hbo.com/movies');
+  } else if (genero === 'accion' && plataforma === 'amazon') {    
+    alert('Te recomendamos películas de acción en Amazon Prime Video. ¡Disfruta!');
+    redirigir('https://www.primevideo.com/-/es/genre/action');
+  } else if (genero === 'comedia' && plataforma === 'netflix') {
+    alert('Te recomendamos comedias en Netflix. ¡Diviértete!');
+    redirigir('https://www.netflix.com/browse/genre/6548');
+  } else if (genero === 'drama' && plataforma === 'cine') {
+    alert('Te recomendamos dramas en el cine. ¡Prepárate para una experiencia emotiva!');
+    redirigir('https://www.google.com/search?q=dramas+en+cine');
+  } else {
+    alert('Gracias por personalizar tu experiencia. ¡Disfruta del cine!');
+  }
+}
+
+// Función para redirigir a la URL proporcionada
+function redirigir(url) {
+  window.location.href = url;
+}
